e9c475aa52d17ccf2a4b62f46d3bc42633ddeb9c
github username <github email address>
2020-08-14 12:56:46 -0400

    new form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,22 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
         @steps = @recipe.instructions.split("\n")
     end
+
+    def new
+    @recipe = Recipe.new
+    end
+
+    def create
+        recipe = Recipe.create(recipe_params)
+        redirect_to recipe_path(recipe)
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:starter_recipe_id, :name, :description, :image_url, :serving_size, :calorie_count, :instructions, :is_starter, :chef_name)
+    end
+
+
+
 end
