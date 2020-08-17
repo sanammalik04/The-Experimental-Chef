248d66b1330f5caa046e8a67668dc5586266b27e
github username <github email address>
2020-08-17 16:36:18 -0400

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token, :only => :create
 
-    def new
+    def login
+    
 
     end
 
-    def create
-        session[:user_name]= params[:user_name]
-        redirect_to user_path()
+    def process_login
+        @user = User.find_by(user_name: params[:user_name])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to recipes_path
+        else
+            flash.now[:login_error] = "Invalid User Name"
+            render :login
+        end
     end
+
+    def logout
+        session.clear
+        redirect_to login_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :user_recipes
   resources :recipes
   resources :users
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#process_login'
+  delete '/logout', to: 'sessions#logout'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
