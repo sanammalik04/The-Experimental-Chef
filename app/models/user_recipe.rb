1ea2717db410bba1a109ce8697c7cb780849fdc5
chsohn15 <chsohn15@gmail.com>
2020-08-17 20:52:46 -0400

    Edited UserRecipe validation

--- app/models/user_recipe.rb
+++ app/models/user_recipe.rb
@@ -2,9 +2,9 @@ class UserRecipe < ApplicationRecord
     belongs_to :recipe
     belongs_to :user
 
-    validates :rating, :inclusion => { :in => 1..10, message: "must be between 1 and 10"}, if: :saved_is_nil?
+    validates :rating, :inclusion => { :in => 1..10, message: "must be between 1 and 10"}, if: :rating_not_nil?
 
-    def saved_is_nil?
-        self.saved == nil
+    def rating_not_nil?
+        self.rating != nil
     end
 end
