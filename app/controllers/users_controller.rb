47a2658e20fdab8944ae5c97d287f9f5a5dd3aa6
chsohn15 <chsohn15@gmail.com>
2020-08-20 12:20:09 -0400

    Formatted Sign-up Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:user_name, :password, :password_confirmation)
+        params.require(:user).permit(:user_name, :password, :password_confirmation, :name)
     end
 
 
